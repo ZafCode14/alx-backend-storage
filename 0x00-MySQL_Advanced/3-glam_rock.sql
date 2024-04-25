c63a22ac58711918faa4e13067b346ccebb7bfb3
ZafCode14 <zafarwp14@gmail.com>
2024-04-25 11:50:11 +0200

    3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,15 @@
 -- SQL script that lists all bands with Glam roch as their main style
-SELECT band_name,
-       (YEAR('2022-01-01') - formed) AS lifespan
-FROM metal_bands
-WHERE split LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+SELECT
+    band_name,
+    CASE
+        WHEN formed > 0 AND split > 0 THEN 2022 - GREATEST(formed, split)
+        WHEN formed > 0 THEN 2022 - formed
+        WHEN split > 0 THEN 2022 - split
+        ELSE 0
+    END AS lifespan
+FROM
+    metal_bands
+WHERE
+    style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
