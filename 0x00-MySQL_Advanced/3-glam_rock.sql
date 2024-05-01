8da31c695a14e0e75f7892b15fe30a4b703941e0
ZafCode14 <zafarwp14@gmail.com>
2024-05-01 22:52:55 +0300

    web

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- SQL script that lists all bands with Glam roch as their main style
+-- SQL script that lists all bands with Glam rock as their main style
 SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
